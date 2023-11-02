546f19cc6be403fc8fca8c35e7ca953cef67a01a
Albertrose <84316640+Anithasadhanala@users.noreply.github.com>
2023-11-02 21:34:38 +0530

    Create trees_AVG_subtree_O(n).cpp

---
+++ trees_AVG_subtree_O(n).cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    int count=0;
+    pair<int,int> postorder(TreeNode*root){
+        pair<int,int>p;
+        if(root==NULL) return p =make_pair(0,-1);
+        pair<int,int> l = postorder(root->left);
+        pair<int,int> h = postorder(root->right);
+        int sum=root->val,c=1;
+        if(l.second!=-1){
+            sum+=l.first;
+            c+=l.second;
+        }
+        if(h.second!=-1){
+            sum+=h.first;
+            c+=h.second;
+        }
+        if(sum/c == root->val) count++;
+        return p= make_pair(sum,c);   
+    }
+
+
+    int averageOfSubtree(TreeNode* root) {
+        postorder(root);
+        return count;
+    }
+};
